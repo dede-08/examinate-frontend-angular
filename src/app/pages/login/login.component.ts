ac5603e0f04241e4f5a54d71f2f0abe57ff73754
dede-08 <benandrealanyaguevara@gmail.com>
2025-07-11 20:49:55 -0500

    mejora en el login

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -69,11 +69,13 @@ export class LoginComponent {
                 //dashboard admin
                 //window.location.href = '/admin';
                 this.router.navigate(['admin']);
+                this.loginService.loginStatusSubject.next(true);
               }
               else if(this.loginService.getUserRole() == "NORMAL"){
                 //user dashboard
                 //window.location.href = '/user-dashboard'
                 this.router.navigate(['user-dashboard']);
+                this.loginService.loginStatusSubject.next(true);
               }
               else{
                 this.loginService.logOut();
